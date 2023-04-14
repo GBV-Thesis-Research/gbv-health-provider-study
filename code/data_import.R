5c6f55eae03937db4d3db7528d8be4f1938f0f0a
Jessica Dyer <jessica.dyer@leveltenenergy.com>
2023-04-13 23:28:34 -0700

    did some linting

--- code/data_import.R
+++ code/data_import.R
@@ -8,10 +8,10 @@
 #### WD SETUP ####
 current_wd <- getwd()
 
-if(endsWith(current_wd, "gbv-health-provider-study")){
+if (endsWith(current_wd, "gbv-health-provider-study")) {
   gbv_project_wd <- current_wd
 } else if (endsWith(current_wd, "/paper")) {
-  gbv_project_wd <- str_remove(current_wd, "/paper")   
+  gbv_project_wd <- str_remove(current_wd, "/paper")
 } else {
   print("Got a WD that's not handled in the If-else ladder yet")
 }
@@ -19,24 +19,22 @@ if(endsWith(current_wd, "gbv-health-provider-study")){
 source(paste(gbv_project_wd, "/keys.R", sep = ""))
 source(paste(gbv_project_wd, "/code/dependencies.R", sep = ""))
 
-import_redcap_data <- function(token_str){
-  redcap_read(redcap_uri='https://redcap.iths.org/api/',
-              token=token_str,
-              raw_or_label = "label",
-              export_checkbox_label = TRUE
+import_redcap_data <- function(token_str) {
+  redcap_read(
+    redcap_uri = "https://redcap.iths.org/api/",
+    token = token_str,
+    raw_or_label = "label",
+    export_checkbox_label = TRUE
   )$data
 }
 
-data_exists = file.exists(paste(gbv_project_wd, "/data/gbv_data_raw.csv", sep = ""))
-if(data_exists){
-  raw_gbv_survey_data = read.csv(paste(gbv_project_wd, "/data/gbv_data_raw.csv", sep = ""))
+data_exists <- file.exists(paste(gbv_project_wd, "/data/gbv_data_raw.csv", sep = ""))
+if (data_exists) {
+  raw_gbv_survey_data <- read.csv(paste(gbv_project_wd, "/data/gbv_data_raw.csv", sep = ""))
 } else {
-  raw_gbv_survey_data = import_redcap_data(token_str = API_KEY)
-  path_to_write = paste(gbv_project_wd, "/data/gbv_data_raw.csv", sep = "")
-  path_to_rds = paste(gbv_project_wd, "/data/gbv_data_raw.RDS", sep = "")
+  raw_gbv_survey_data <- import_redcap_data(token_str = API_KEY)
+  path_to_write <- paste(gbv_project_wd, "/data/gbv_data_raw.csv", sep = "")
+  path_to_rds <- paste(gbv_project_wd, "/data/gbv_data_raw.RDS", sep = "")
   write.csv(raw_gbv_survey_data, path_to_write)
   saveRDS(raw_gbv_survey_data, file = path_to_rds)
 }
-
-print("I'm making a change!")
-
--- code/dependencies.R
+++ code/dependencies.R
@@ -1,7 +1,7 @@
-packages <- c("redcapAPI", "REDCapR")
+packages <- c("redcapAPI", "REDCapR", "lintr", "styler")
 
-new.packages <- packages[!(packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
+new.packages <- packages[!(packages %in% installed.packages()[, "Package"])]
+if (length(new.packages)) install.packages(new.packages)
 
 # Load packages
 invisible(lapply(packages, library, character.only = TRUE))
