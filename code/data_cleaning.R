39644462c404ffac9ef9ebfac9c082d0d6d3a2c9
Jessica Dyer <jessica.dyer@leveltenenergy.com>
2023-07-17 17:29:01 -0700

    updated comment.

--- code/data_cleaning.R
+++ code/data_cleaning.R
@@ -46,8 +46,8 @@ data <- data %>%
 # Drops from 929 to 416 (removes 513 rows)
 columns_to_not_select <- ("record_id")
 all_columns <- colnames(data)
-# There are times when the X does not exist in the data. This is to handle that
-# case.
+# When the data is first imported, X does not exist in the data. However, it 
+# exists later. This is to handle that case.
 if ("X" %in% all_columns) {
   columns_to_not_select <- c(columns_to_not_select, "X")
 }
