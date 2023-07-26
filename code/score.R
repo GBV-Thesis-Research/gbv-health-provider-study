1004222e021206bccee0338242bcc7c5178c3153
Susan Glenn <susancg@uw.edu>
2023-07-26 11:45:57 -0700

    confidence rescale

--- code/score.R
+++ code/score.R
@@ -33,9 +33,13 @@ key_only <- key %>%
 #' Attitudes
 #' Modify data based on key values
 #'
-#' This code modifies data based on key values for each variable in attitude vars.
+#' This code modifies data based on key values for each variable in attitude vars. The
+#' questions were phrased in such a way that the desired response was either 1 or 5,
+#' depending on the question. Given that higher scores indicate the desired attitudes
+#' toward GBV, we wanted all desired answers to be scored as t he highest possible score (4).
 #' If the key value is 5, the variable is scaled down by 1 (0-4). If the key value is 1,
-#' the variable is reversed coded.
+#' the variable is reversed coded. After subtracting 1 from all scores, score 3 (response "I don't know")
+#' was re-scored to a 1 (the same score as "sometimes acceptable")
 
 # Get column names matching 'attitudes'
 att_vars <- names(data)[str_detect(names(data), "attitudes")]
@@ -43,7 +47,9 @@ att_vars <- names(data)[str_detect(names(data), "attitudes")]
 # Get column names matching 'attitudes_12' from att_vars
 att12_vars <- att_vars[str_detect(att_vars, "attitudes_12")]
 
-# Remove 'attitudes_12' column names from att_vars
+# Remove 'attitudes_12' column names from att_vars.
+# Question 12 was removed from att_vars as it only had 4 possible answers, compared to
+# 5 in the rest of the attitude domain questions. Question 12 will be evaluated separately.
 att_vars <- att_vars[!str_detect(att_vars, "attitudes_12")]
 
 clean_data <- clean_data %>%
@@ -59,6 +65,25 @@ clean_data <- clean_data %>%
     across(all_of(att12_vars), ~ . - 1),
     across(all_of(att12_vars), ~ ifelse(. == 3, 1, .))
   )
+
+#' Confidence
+#' Rescale to a 0-4 scale
+#'
+#' This code modified data based on key values for each variable in attitude vars.
+#' All items are positively worded, so we moved scale down by 1, with highest confidence
+#' score being 4, and low confidence being 0.
+#' (Cory said 3 = correct answer? Ask if this was a mistake / run the numbers to check)
+
+conf_vars <- names(clean_data)[str_detect(names(data), "confidence")]
+
+clean_data <- clean_data %>% mutate(across(all_of(conf_vars), ~ case_when(
+  conf_vars = 1:5 ~ . - 1)))
+
+#' Empathy
+#' Modify data based on key values
+#'
+#' 
+
 
 # Scoring (might only want to include knowledge & system support here?)
 knowledge_sys_support_key <- key %>%
