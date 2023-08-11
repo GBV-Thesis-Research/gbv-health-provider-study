531fd516a2c2d3d585c25cac74d77614d5794556
Susan Glenn <susancg@uw.edu>
2023-08-10 21:54:20 -0700

    add code for separating key from data and dropping participants who didn't consent to research

--- code/data_cleaning.R
+++ code/data_cleaning.R
@@ -23,23 +23,6 @@ source(paste(gbv_project_wd, "/code/data_import.R", sep = ""))
 style_file(paste(gbv_project_wd, "/code/data_cleaning.R", sep = ""))
 
 # DATA MANAGEMENT --------------------------------------------------------------
-# Separate out key from data (key is the redcap entry form containing correct answers)
-key <- raw_gbv_survey_data %>%
-  filter(participant_id == "KEY") %>%
-  select(everything())
-
-data <- raw_gbv_survey_data %>%
-  mutate(time_point = if_else(date %in% c("2003-07-03", "2003-07-10", "2017-07-10", "2023-07-03", "2023-07-10"), 3, time_point)) %>%
-  filter(time_point != 3)
-
-# Drop participants that have not consented to have their data used for research
-# Drops from 972 to 929 (removes 43 rows)
-data <- data %>%
-  filter(consent == 1) %>%
-  filter(!date %in% c("2021-06-18", "2021-06-14")) %>%
-  filter(municipality != "Dili") %>%
-  filter(participant_id != "KEY")
-
 # Standardize municipality names
 data <- data %>%
   mutate(municipality = if_else(municipality %in% c("LIQUICA", "Liquisa", "LIQUISA", "Liqujca"),
