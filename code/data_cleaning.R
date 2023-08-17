2d4cf09a11eed8a8c80864c1c53ce5704d5f5794
Jessica Dyer <jessica.dyer@leveltenenergy.com>
2023-08-16 21:44:58 -0700

    finished almost all of the facilities in the standardization

--- code/data_cleaning.R
+++ code/data_cleaning.R
@@ -23,18 +23,23 @@ source(paste(gbv_project_wd, "/code/data_import.R", sep = ""))
 style_file(paste(gbv_project_wd, "/code/data_cleaning.R", sep = ""))
 
 # DATA MANAGEMENT --------------------------------------------------------------
+path_to_facility_file <- paste(gbv_project_wd, "/data/extra_data/facility_names.xlsx", sep = "")
+
+standard_facility_names <- read_excel(path_to_facility_file, sheet = "facilities")
+standard_facility_names <- standard_facility_names %>% mutate(full_name = paste(type, name, sep = " "))
+
 # Separate out key from data (key is the redcap entry form containing correct answers)
 key <- raw_gbv_survey_data %>%
   filter(participant_id == "KEY") %>%
   select(everything())
 
-# data <- raw_gbv_survey_data %>%
-#   mutate(time_point = if_else(date %in% c("2003-07-03", "2003-07-10", "2017-07-10", "2023-07-03", "2023-07-10"), 3, time_point)) %>%
-#   filter(time_point != 3)
+data <- raw_gbv_survey_data %>%
+  mutate(time_point = if_else(date %in% c("2003-07-03", "2003-07-10", "2017-07-10", "2023-07-03", "2023-07-10"), 3, time_point)) %>%
+  filter(time_point != 3)
 
 # Drop participants that have not consented to have their data used for research
 # Drops from 972 to 929 (removes 43 rows)
-data <- raw_gbv_survey_data %>%
+data <- data %>%
   filter(consent == 1) %>%
   filter(!date %in% c("2021-06-18", "2021-06-14")) %>%
   filter(municipality != "Dili") %>%
@@ -184,51 +189,29 @@ data <- data %>%
 data <- data %>%
   select(-starts_with("practices_19"))
 
-# Standardize facility name
-drop_letters_before_longer_part <- function(string) {
-  longest_part <- gsub("^.* ", "", string) # Extract the longer part
-  return(toupper(longest_part))
-}
+data <- data %>%
+  mutate(facility_names_no_beginning = map(facility, str_to_title)) %>%
+  mutate(facility = ifelse(facility_names_no_beginning %in% c("Ps Asulau"), "Asulau Sare", facility)) %>%
+  mutate(facility = ifelse(facility_names_no_beginning %in% c("Ps Vatunau Ediri"), "Ediri", facility)) %>%
+  mutate(facility = ifelse(facility_names_no_beginning %in% c("Postu Saude Leotela"), "Leotala", facility)) %>%
+  mutate(facility = ifelse(facility_names_no_beginning %in% c("Chcguisaduru"), "Guissarudo", facility)) %>%
+  mutate(facility = ifelse(facility_names_no_beginning %in% c("Ps Bakhita"), "Bakita Eraulo", facility)) %>%
+  mutate(facility = ifelse(facility_names_no_beginning %in% c("Hp Leofela"), "Leotala", facility)) %>%
+  mutate(facility = ifelse(facility_names_no_beginning %in% c("Ps Falihbo"), "Fahilebu", facility))
 
 data <- data %>%
-  mutate(facility = ifelse(facility %in% c(
-    "POSTO SAUDE", "POSTU DA SAUDE", "JS200921", "CHC Centru Saude", "SSAM / DHS",
-    "HP", "Cgc"
-  ), "DELETE", facility)) %>%
-  mutate(facility = ifelse(facility %in% c("PS ASULAU SARE", "CHC Asulau Sare"), "ASULAU", facility)) %>%
-  mutate(facility = ifelse(facility %in% c("HP Hatulia B"), "HATULIA", facility)) %>%
-  mutate(facility = ifelse(facility %in% c("HP Açumanu"), "ACUMANO", facility)) %>%
-  mutate(facility = ifelse(facility %in% c("CHC Raila o"), "RAILA", facility)) %>%
-  mutate(facility = ifelse(facility %in% c("PS VATUNAU EDIR"), "VATUNAU", facility))
-
-standard_facility_names <- c(
-  "ACUMANO", "AIMETA", "ASULAU", "ASUMANU", "ATSABE", "BAKHITA", "BAURA",
-  "BAZARTETE", "BUISADURO", "CAICASSA", "GUISADURU", "DARULETE", "DEHO", "DELECO",
-  "EBENU", "EDIRI", "EKAPU", "ERMERA", "ESTADO", "FAHILEBU", "FATUBESSI",
-  "FATUQUERO", "GLENO", "GOULOLO", "GUICU", "GUISARUDO",
-  "HATULIA", "HATULAILETE", "HATUGAU", "HATUHEI", "HATUQUESI",
-  "INTERNAMENTU", "LEIMEA KRAIK", "LADODO", "LAUHATA", "LEBUTELU", "LEIMEA",
-  "LEOTELA", "LETEFOHO", "LEMEA LETEN", "LEUBASA", "LICAPAT", "LISAIKO",
-  "LIQUICA", "LEQUISALA", "LISSA", "LODUDO", "LOIDAHAR", "MANULETE", "PARAMI", "PONILALA",
-  "MANUSAE", "MATATA", "MAUBARA", "MUBARA LISA", "MAUMETAK", "MOTAULUN", "METAGOU",
-  "RAILA", "RAEGOA", "RAEMETA", "RAENABA", "RAERAGA", "RAILAKO LETEN", "RAIMETA", "VATUBORO",
-  "SIAMODO", "TATSABE", "TIBAR", "ULMERA",
-  "VATUNAU", "ULUANA"
-)
-
-data <- data %>%
-  mutate(facility_names_no_beginning = lapply(facility, drop_letters_before_longer_part)) %>%
   mutate(
     standardized_facility = map_chr(facility_names_no_beginning, function(fac) {
-      dist_matrix <- stringdist::stringdistmatrix(fac, standard_facility_names)
-      closest_standard <- standard_facility_names[which.min(dist_matrix)]
+      dist_matrix <- stringdist::stringdistmatrix(fac, standard_facility_names$full_name)
+      closest_standard <- standard_facility_names$full_name[which.min(dist_matrix)]
       return(closest_standard)
     })
   ) %>%
-  # select(facility, standardized_facility) %>%
-  arrange(standardized_facility) %>%
-  mutate(standardized_facility = ifelse(standardized_facility %in% c("DELETE"), NULL, standardized_facility))
-
+  # select(facility, facility_names_no_beginning, standardized_facility) %>%
+  mutate(standardized_facility = ifelse(facility_names_no_beginning %in% c(
+    "Ssam / Dhs", "Ps Estado", "Postu Da Saude", "Chc Centru Saude", "Js200921", "Hp", "Posto Saude", "Cgc"
+  ), NA, standardized_facility)) %>%
+  relocate(standardized_facility, .after = facility)
 # Write data to folder
 path_to_clean_rds <- paste(gbv_project_wd, "/data/clean/gbv_data_interim_clean.RDS", sep = "")
 saveRDS(data, file = path_to_clean_rds)
