b15bb982e776d0d1d9ada30e60e0df1c63ca2493
Susan Glenn <susancg@uw.edu>
2023-09-10 18:27:23 -0700

    probably a bunch of useless code because i can't figure out how to make these tables

--- code/mel_report_analysis.R
+++ code/mel_report_analysis.R
@@ -32,12 +32,12 @@ clean_data_scores <- clean_data_scores %>%
 clean_data_scores <- clean_data_scores %>%
   mutate(outcome4_pre_score = ifelse(time_point == 1,
                                      ((knowledge_general_score + knowledge_warning_score +
-                                         knowledge_appropriate_score + knowledge_helpful_score +
+                                       knowledge_appropriate_score + knowledge_helpful_score +
                                          attitude_general_score + attitude_acceptability_score +
                                          attitude_genderroles_score + attitude_profroles_score +
                                          empathy_score) / 900) * 100,
                                      NA))
-
+#
 # Create new follow-up-score variable for outcome 4, including the knowledge, attitude, and empathy domains
 clean_data_scores <- clean_data_scores %>%
   mutate(outcome4_post_score = ifelse(time_point == 3,
@@ -48,129 +48,101 @@ clean_data_scores <- clean_data_scores %>%
                                          empathy_score) / 900) * 100,
                                      NA))
 
-# Create new pre-score variable for outcome 4, including the confidence, system support, 
-# and professional role domains <- have to ask Xylia  about the professional role one here 
-# as its in attitude domain, not its own domain
+# # Create new pre-score variable for outcome 4, including the confidence, system support,
+# # and professional role domains <- have to ask Xylia  about the professional role one here
+# # as its in attitude domain, not its own domain
 clean_data_scores <- clean_data_scores %>%
   mutate(outcome5_pre_score = ifelse(time_point == 1,
                                      ((confidence_score + system_support_score) / 200) * 100,
                                      NA))
 
-# Create new follow-up score variable for outcome 4, including the confidence, system support, 
-# and professional role domains <- have to ask Xylia  about the professional role one here 
-# as its in attitude domain, not its own domain
+# # Create new follow-up score variable for outcome 4, including the confidence, system support,
+# # and professional role domains <- have to ask Xylia  about the professional role one here
+# # as its in attitude domain, not its own domain
 clean_data_scores <- clean_data_scores %>%
   mutate(outcome5_post_score = ifelse(time_point == 3,
                                      ((confidence_score + system_support_score) / 200) * 100,
                                      NA))
 
-# CREATE NEW DATAFRAME FOR MEAN REGIONAL SCORES --------------------------------
-# Create new data frame for mean regional domain scores
-regional_scores_mean <- clean_data_scores %>%
-  group_by(region) %>%
-  summarize(
-    outcome4_pre_score = mean(outcome4_pre_score, na.rm = TRUE),
-    outcome4_post_score = mean(outcome4_post_score, na.rm = TRUE),
-    outcome5_pre_score = mean(outcome5_pre_score, na.rm = TRUE),
-    outcome5_post_score = mean(outcome5_post_score, na.rm = TRUE),
-  )
-
-# Transpose regional_scores_mean table
-regional_scores_mean <- t(regional_scores_mean) %>% 
-  as.data.frame(regional_scores_mean)
-
-# Rename columns to regions & drop region row from dataframe
-colnames(regional_scores_mean) <- regional_scores_mean[1, ] 
-regional_scores_mean <- regional_scores_mean[-1, ]
-
-# Rename rows to outcomes pre/post 
-row.names(regional_scores_mean) <- c("Outcome 4 pre", "Outcome 4 post", "Outcome 5 pre", "Outcome 5 post")
-
-# CREATE NEW DATAFRAME FOR MEAN FACILITY SCORES --------------------------------
-# Create new data frame for mean facility domain scores
-facility_scores_mean <- clean_data_scores %>%
-  group_by(standardized_facility) %>%
-  summarize(
-    outcome4_pre_score = mean(outcome4_pre_score, na.rm = TRUE),
-    outcome4_post_score = mean(outcome4_post_score, na.rm = TRUE),
-    outcome5_pre_score = mean(outcome5_pre_score, na.rm = TRUE),
-    outcome5_post_score = mean(outcome5_post_score, na.rm = TRUE),
-  )
-
-# Transpose regional_scores_mean table
-facility_scores_mean <- t(facility_scores_mean) %>% 
-  as.data.frame(facility_scores_mean)
-
-# Rename columns to regions & drop region row from dataframe
-colnames(facility_scores_mean) <- regional_scores_mean[1, ] 
-facility_scores_mean <- facility_scores_mean[-1, ]
-
-# Rename rows to outcomes pre/post 
-row.names(facility_scores_mean) <- c("Outcome 4 pre", "Outcome 4 post", "Outcome 5 pre", "Outcome 5 post")
-
-# CREATE NEW DATAFRAMES FOR MEDIAN REGIONAL SCORES------------------------------
-# Create new data frame for median regional domain scores
+# # CREATE NEW DATAFRAMES FOR MEDIAN REGIONAL SCORES------------------------------
+# # Create new data frame for median regional domain scores
 regional_scores_median <- clean_data_scores %>%
   group_by(region) %>%
   summarize(
     outcome4_pre_score = median(outcome4_pre_score, na.rm = TRUE),
-    outcome4_post_score = median(outcome4_post_score, na.rm = TRUE),
     outcome5_pre_score = median(outcome5_pre_score, na.rm = TRUE),
+    outcome4_post_score = median(outcome4_post_score, na.rm = TRUE),
     outcome5_post_score = median(outcome5_post_score, na.rm = TRUE),
   )
 
 # Transpose regional_scores_mean table
-regional_scores_median <- t(regional_scores_median) %>% 
-  as.data.frame(regional_scores_median)
+regional_scores_median_pre <- t(regional_scores_median_pre) %>%
+  as.data.frame(regional_scores_median_pre)
+
+regional_scores_median_post <- t(regional_scores_median_post) %>%
+  as.data.frame(regional_scores_median_post)
 
 # Rename columns to regions & drop region row from dataframe
-colnames(regional_scores_median) <- regional_scores_median[1, ] 
-regional_scores_median <- regional_scores_median[-1, ]
+colnames(regional_scores_median_pre) <- regional_scores_median_pre[1, ]
+regional_scores_median_pre <- regional_scores_median_pre[-1, ]
 
-# Rename rows to outcomes pre/post 
-row.names(regional_scores_median) <- c("Outcome 4 pre", "Outcome 4 post", "Outcome 5 pre", "Outcome 5 post")
+colnames(regional_scores_median_post) <- regional_scores_median_post[1, ]
+regional_scores_median_post <- regional_scores_median_post[-1, ]
 
-# CREATE NEW DATAFRAMES FOR MEDIAN FACILITY SCORES------------------------------
+# Rename rows to outcomes pre/post
+row.names(regional_scores_median_pre) <- c("Outcome 4 pre", "Outcome 5 pre")
 
-# Create new data frame for median facility domain scores
-facility_scores_median <- clean_data_scores %>%
-  group_by(standardized_facility) %>%
-  summarize(
-    outcome4_pre_score = median(outcome4_pre_score, na.rm = TRUE),
-    outcome4_post_score = median(outcome4_post_score, na.rm = TRUE),
-    outcome5_pre_score = median(outcome5_pre_score, na.rm = TRUE),
-    outcome5_post_score = median(outcome5_post_score, na.rm = TRUE),
-  )
+row.names(regional_scores_median_post) <- c("Outcome 4 post", "Outcome 5 post")
 
-# Transpose regional_scores_mean table
-facility_scores_median <- t(facility_scores_median) %>% 
-  as.data.frame(facility_scores_median)
+# Make df numeric
+for (col in names(regional_scores_median_pre)) {
+  regional_scores_median_pre[[col]] <- as.numeric(regional_scores_median_pre[[col]])
+}
+str(regional_scores_median_pre)
 
-# Rename columns to regions & drop region row from dataframe
-colnames(facility_scores_median) <- facility_scores_median[1, ] 
-facility_scores_median <- facility_scores_median[-1, ]
+for (col in names(regional_scores_median_post)) {
+  regional_scores_median_post[[col]] <- as.numeric(regional_scores_median_post[[col]])
+}
+str(regional_scores_median_post)
 
-# Rename rows to outcomes pre/post 
-row.names(facility_scores_median) <- c("Outcome 4 pre", "Outcome 4 post", "Outcome 5 pre", "Outcome 5 post")
+# 
+result <- wilcox.test(group1, group2, paired = TRUE)
 
-# CREATE MEDIAN SCORE TABLES FOR REGIONAL SCORES BASED ON MEL PLAN ---------------
-# Create score tables - outcome 4
-outcome4_scores_summary_table <- regional_scores_median %>%
-  tbl_summary(
-    by = c(starts_with("outcome4")),
-    type = c(
-      region
-    ) ~ "continuous",
-    label = list(
-      outcome4_pre_score ~ "Outcome 4: knowledge, empathy, and attitudes pre-score",
-      outcome4_post_score ~ "Outcome 4: knowledge, empathy, and attitudes pre-score"
-    ),
-    statistic = list(
-      all_continuous() ~ "{median} ({sd})"
-    ),
-    digits = all_continuous() ~ 2
-  ) %>%
-  add_p() %>%
-  add_n() %>%
-  bold_p()
-outcome4_scores_summary_table
+# CREATE NEW DATAFRAMES FOR MEDIAN REGIONAL SCORES FOR OUTCOME 4----------------
+# Create new data frame for median regional domain scores - pre-test
+# regional_scores_median_pre <- clean_data_scores %>%
+#   filter(time_point == 1) %>%
+#   group_by(region) %>%
+#   summarize(
+#     knowledge_general_score = median(knowledge_general_score),
+#     knowledge_warning_score = median(knowledge_warning_score),
+#     knowledge_helpful_score = median(knowledge_helpful_score),
+#     knowledge_appropriate_score = median(knowledge_appropriate_score),
+#     attitude_general_score = median(attitude_general_score),
+#     attitude_acceptability_score = median(attitude_acceptability_score),
+#     attitude_genderroles_score = median(attitude_genderroles_score),
+#     empathy_score = median(empathy_score)
+#   )
+# 
+# #regional_scores_median_pre <- regional_scores_median_pre %>% mutate(timepoint = "1")
+# 
+# # Create new data frame for median regional domain scores - post-test
+# regional_scores_median_post <- clean_data_scores %>%
+#   filter(time_point == 3) %>%
+#   group_by(region) %>%
+#   summarize(
+#     knowledge_general_score = median(knowledge_general_score),
+#     knowledge_warning_score = median(knowledge_warning_score),
+#     knowledge_helpful_score = median(knowledge_helpful_score),
+#     knowledge_appropriate_score = median(knowledge_appropriate_score),
+#     attitude_general_score = median(attitude_general_score),
+#     attitude_acceptability_score = median(attitude_acceptability_score),
+#     attitude_genderroles_score = median(attitude_genderroles_score),
+#     empathy_score = median(empathy_score)
+#   )
+
+#regional_scores_median_post <- regional_scores_median_post %>% mutate(timepoint = "3")
+
+# Merge dataframes
+regional_scores_median <- merge(regional_scores_median_pre, regional_scores_median_post, all = TRUE)
+
