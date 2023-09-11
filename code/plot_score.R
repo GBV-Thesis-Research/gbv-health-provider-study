162c1c63d561ea0c31108d55c1b921683b2c71c0
Susan Glenn <susancg@uw.edu>
2023-09-10 21:32:32 -0700

    set-up new plot document

---
+++ code/plot_score.R
@@ -0,0 +1,33 @@
+##################################################
+## Project: GBV
+## Script purpose: Plot for scores across 3 timepoints
+## Date: 9-10-23
+## Author: Susan Glenn
+##################################################
+
+# NOTE: https://www.danieldsjoberg.com/gtsummary/articles/gallery.html#paired-test
+
+#### WD SETUP ####
+current_wd <- getwd()
+
+if (endsWith(current_wd, "gbv-health-provider-study")) {
+  gbv_project_wd <- current_wd
+} else if (endsWith(current_wd, "/paper")) {
+  gbv_project_wd <- str_remove(current_wd, "/paper")
+} else {
+  print("Got a WD that's not handled in the If-else ladder yet")
+}
+path_to_clean_rds_scores <- paste(gbv_project_wd, "/data/clean/gbv_data_scores.RDS", sep = "")
+
+source(paste(gbv_project_wd, "/code/dependencies.R", sep = ""))
+
+if (!file.exists(path_to_clean_rds_scores)) {
+  source(paste(gbv_project_wd, "/code/score.R", sep = ""))
+}
+
+#### Lint current file ####
+style_file(paste(gbv_project_wd, "/code/table_scores.R", sep = ""))
+
+# Load cleaned data
+path_to_clean_rds_scores <- paste(gbv_project_wd, "/data/clean/gbv_data_scores.RDS", sep = "")
+clean_scores <- readRDS(path_to_clean_rds_scores)
