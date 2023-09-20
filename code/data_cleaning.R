fb4d27600f522d75f2563fafc4749e7b96475219
Susan Glenn <susancg@uw.edu>
2023-09-19 20:07:25 -0700

    adding new variable for chc catchment area

--- code/data_cleaning.R
+++ code/data_cleaning.R
@@ -213,10 +213,12 @@ data <- data %>%
 
 # Create new column for CHCs & the health posts in their region (called "CHC_catchment)
 data <- data %>%
-  mutate(CHC_catchment = ifelse(standardized_facility %in% c( "CHC Gleno", "HP Lauala Leten", "HP Aituro", "HP Huitasu"), "CHC Gleno", .)) %>%
-  mutate(CHC_catchment = ifelse(standardized_facility %in% c( "CHC Ermera", "HP Ponilala", "HP Lodudu"), "CHC Ermera", .)) %>%
-  mutate(CHC_catchment = ifelse(standardized_facility %in% c( "CHC Railaco", "HP Railaco Leten", "HP Matata", "HP Delesu", "HP Samalete", 
-                                                              "HP Terasu"), "CHC Railaco", .))
+  mutate(CHC_catchment = ifelse(standardized_facility %in% c("CHC Gleno", "HP Lauala Leten", "HP Aituro", "HP Huitasu"), "CHC Gleno", .)) %>%
+  mutate(CHC_catchment = ifelse(standardized_facility %in% c("CHC Ermera", "HP Ponilala", "HP Lodudu"), "CHC Ermera", .)) %>%
+  mutate(CHC_catchment = ifelse(standardized_facility %in% c(
+    "CHC Railaco", "HP Railaco Leten", "HP Matata", "HP Delesu", "HP Samalete",
+    "HP Terasu"
+  ), "CHC Railaco", .))
 
 # Write data to folder
 path_to_clean_rds <- paste(gbv_project_wd, "/data/clean/gbv_data_interim_clean.RDS", sep = "")
