92e1755afc66299d83ab08b004af76edeedd784a
Susan Glenn <susancg@uw.edu>
2024-01-14 14:35:22 -0800

    wow, finally fixed the converting to wide format

--- code/analysis.R
+++ code/analysis.R
@@ -25,41 +25,22 @@ path_to_clean_rds <- paste(gbv_project_wd, "/data/clean/gbv_data_clean.RDS", sep
 merged_scores <- readRDS(path_to_clean_rds_scores)
 
 # CREATE NEW DATA FRAME FOR ANALYSIS  ------------------------------------------
-# Pull in domain scores for matched individuals and create variables for timepoint 1, 2, and 3 scores
+# Pull in domain scores for matched individuals and convert to wide format
 analysis <- merged_scores %>%
   filter(status == "All three") %>%
   select(participant_id_3, time_point, knowledge_overall, attitude_overall, system_support_score, 
          confidence_score, empathy_score, practice_score) %>% 
-  mutate(knowledge1 = ifelse(time_point==1, knowledge_overall, NA),
-         knowledge2 = ifelse(time_point==2, knowledge_overall, NA),
-         knowledge3 = ifelse(time_point==3, knowledge_overall, NA),
-         attitude1 = ifelse(time_point==1, attitude_overall, NA),
-         attitude2 = ifelse(time_point==2, attitude_overall, NA),
-         attitude3 = ifelse(time_point==3, attitude_overall, NA),
-         empathy1 = ifelse(time_point==1, empathy_score, NA),
-         empathy2 = ifelse(time_point==2, empathy_score, NA),
-         empathy3 = ifelse(time_point==3, empathy_score, NA),
-         confidence1 = ifelse(time_point==1, confidence_score, NA),
-         confidence2 = ifelse(time_point==2, confidence_score, NA),
-         confidence3 = ifelse(time_point==3, confidence_score, NA),
-         confidence1 = ifelse(time_point==1, confidence_score, NA),
-         system_support_score1 = ifelse(time_point==1, system_support_score, NA),
-         system_support_score2 = ifelse(time_point==2, system_support_score, NA),
-         system_support_score3 = ifelse(time_point==3, system_support_score, NA),
-         practice1 = ifelse(time_point==1, practice_score, NA),
-         practice2 = ifelse(time_point==2, practice_score, NA),
-         practice3 = ifelse(time_point==3, practice_score, NA),
-  ) %>%
-  select(-knowledge_overall, -attitude_overall, -empathy_score, -practice_score, 
-       -confidence_score, -system_support_score)
-         
-# Convert to wide format  
-analysis <- analysis %>%
-    pivot_wider(names_from = time_point, values_from = .) %>%
-    group_by(participant_id_3)
+  pivot_wider(id_cols = participant_id_3, names_from = time_point, values_from = c(knowledge_overall,
+                                                                                   attitude_overall,
+                                                                                   empathy_score,
+                                                                                   confidence_score,
+                                                                                   system_support_score,
+                                                                                   practice_score))
+
 
 # Join attendance data
   
+
 # Join demographics
   
 
