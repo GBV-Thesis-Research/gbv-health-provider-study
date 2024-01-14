a62f677a2c91d30bdc59daaf78ee1e70d47d17c4
Susan Glenn <susancg@uw.edu>
2024-01-14 13:43:37 -0800

    edited a comment

--- code/analysis.R
+++ code/analysis.R
@@ -25,16 +25,11 @@ path_to_clean_rds <- paste(gbv_project_wd, "/data/clean/gbv_data_clean.RDS", sep
 merged_scores <- readRDS(path_to_clean_rds_scores)
 
 # CREATE NEW DATA FRAME FOR ANALYSIS  ------------------------------------------
-# Pull in domain scores for matched individuals and create timepoint 1, 2, and 3 scores
+# Pull in domain scores for matched individuals and create variables for timepoint 1, 2, and 3 scores
 analysis <- merged_scores %>%
   filter(status == "All three") %>%
   select(participant_id_3, time_point, knowledge_overall, attitude_overall, system_support_score, 
          confidence_score, empathy_score, practice_score) %>% 
-  pivot_wider(names_from = time_point, values_from = .) %>%
-                group_by(participant_id_3))
-  
-  
-  
   mutate(knowledge1 = ifelse(time_point==1, knowledge_overall, NA),
          knowledge2 = ifelse(time_point==2, knowledge_overall, NA),
          knowledge3 = ifelse(time_point==3, knowledge_overall, NA),
@@ -61,7 +56,7 @@ analysis <- merged_scores %>%
 # Convert to wide format  
 analysis <- analysis %>%
     pivot_wider(names_from = time_point, values_from = .) %>%
-    group_by(participant_id_3))
+    group_by(participant_id_3)
 
 # Join attendance data
   
