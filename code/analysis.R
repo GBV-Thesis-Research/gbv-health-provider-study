f543ab7c7cdffbbc0e7b1f60516128e78b11f66e
Susan Glenn <susancg@uw.edu>
2024-02-04 13:20:41 -0800

    cleaning up regression scripts. This script still needs a lot of work, but waiting for more direction after my meeting with Mary

--- code/analysis.R
+++ code/analysis.R
@@ -23,6 +23,7 @@ df_wide <- readRDS(analysis_df_fp_wide)
 analysis_df_fp_long <- paste(gbv_project_wd, "/data/clean/analysis_data_long.RDS", sep = "")
 df_long <- readRDS(analysis_df_fp_long)
 
+# Score comparison table (at timepoints 1 & 3)
 comparison_tbl_1 <-
   df_long %>%
   filter(time_point %in% c(1, 3)) %>%
@@ -37,13 +38,12 @@ comparison_tbl_1 <-
 #load rigr
 library(rigr)
 
-# Check distribution
+# Check distributions - do I need to do this for each domain at each timepoint?
 hist(df_wide$knowledge_overall_1, freq = TRUE)
 hist(df_wide$knowledge_overall_2, freq = TRUE)
 hist(df_wide$knowledge_overall_3, freq = TRUE)
 
-
-# create variable for attendance at FUAT alone (not intensive training)
+# create variable for attendance at FUAT alone (not GBV intensive training)
 df_wide <- df_wide %>%
   mutate(
     attendance_fuat = attendance_score - 1
@@ -53,7 +53,7 @@ df_wide <- df_wide %>%
 df_wide$position_groups <- relevel(df_wide$position_groups, ref = "Medical doctor")
 df_wide$age_groups <- relevel(df_wide$age_groups, ref = "Less than 25 years old")
 
-# bivariate analysis for additive effects
+# Conduct bivariate analysis for additive effects
 library(jtools)
 lm_sex <- lm(knowledge_overall_3 ~ sex_factored, data = df_wide)
 summ(lm_sex)
@@ -71,7 +71,7 @@ lm_attendance <- lm(knowledge_overall_3 ~ attendance_fuat, data = df_wide)
 summ(lm_attendance)
 confint(lm_attendance)
 
-# linear regression, adjusting for baseline
+# linear regression, adjusting for baseline scores only
 knowledge_reg <- lm(knowledge_overall_3 ~ knowledge_overall_2, data = df_wide)
 summ(knowledge_reg)
 confint(knowledge_reg)
@@ -92,7 +92,7 @@ sys_reg <- lm(system_support_score_3 ~ system_support_score_2 +  system_support_
 summ(sys_reg)
 confint(sys_reg)
 
-# linear regression, adjusting for confounders
+# linear regression, adjusting for characteristics
 example_regession <- lm(knowledge_overall_3 ~ sex_factored + attendance_fuat + 
                           age_groups + position_groups + knowledge_overall_1, data = df_wide)
 summ(example_regession)
@@ -101,50 +101,3 @@ example_regession <- lm(knowledge_overall_3 ~ sex_factored + attendance_fuat +
                           age_groups + position_groups + knowledge_overall_1 + 
                           knowledge_overall_2, data = df_wide)
 summ(example_regession)
-
-# create new improvement variables
-df_wide <- df_wide %>%
-  mutate(improvement_knowledge_overall =
-           ifelse(knowledge_overall_3 > knowledge_overall_1, 1, 0))
-
-df_wide <- df_wide %>%
-  mutate(improvement_attitude_midline =
-           ifelse(attitude_overall_3 > attitude_overall_2, 1, 0))
-
-df_wide <- df_wide %>%
-  mutate(improvement_attitude_overall =
-           ifelse(attitude_overall_3 > attitude_overall_1, 1, 0))
-
-# Logistic regression with improvement variables
-logregession_knowledge <- glm(improvement_knowledge_overall ~ sex_factored + 
-                                  attendance_score + age_groups + position_groups + 
-                                  knowledge_overall_1, 
-                              data = df_wide)
-exp(coef(logregession_knowledge))
-exp(confint(logregession_knowledge))
-summary(logregession_knowledge)
-
-logregession_knowledge_midline_exp <- glm(improvement_attitude_midline ~ sex_factored + 
-                                        attendance_score + age_groups + position_groups + 
-                                        knowledge_overall_1, 
-                                      data = df_wide)
-exp(coef(logregession_knowledge_midline_exp))
-exp(confint(logregession_knowledge_midline_exp))
-
-example_regession_attitudes <- glm(improvement_attitude_overall ~ sex_factored + 
-                                     attendance_score + age_groups + position_groups + 
-                                     knowledge_overall_1, 
-                                   data = df_wide)
-exp(coef(example_regession_attitudes))
-exp(confint(example_regession_attitudes)) 
-
-# install.packages("logistf")
-# library(logistf)
-# model <- logistf(improvement_attitude_overall ~ sex_factored + 
-#                    attendance_score + age_groups + position_groups + 
-#                    knowledge_overall_1, 
-#                  data = df_wide,
-#                  family = binomial)
-# 
-# exp(coef(model))
-# exp(confint(model)) 
