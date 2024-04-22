53c6ce0fe8e2e8a3afd8168e6ca07300990cdd0b
Susan Glenn <susancg@uw.edu>
2024-04-21 18:52:40 -0700

    updating score tables with total points

--- code/analysis.R
+++ code/analysis.R
@@ -17,6 +17,8 @@ if (endsWith(current_wd, "gbv-health-provider-study")) {
   print("Got a WD that's not handled in the If-else ladder yet")
 }
 
+source(paste(gbv_project_wd, "/code/dependencies.R", sep = ""))
+
 source(paste(gbv_project_wd, "/code/analysis_prep.R", sep = ""))
 style_file(paste(gbv_project_wd, "/code/analysis_prep.R", sep = ""))
 
@@ -32,28 +34,25 @@ comparison_tbl_1 <-
   filter(time_point %in% c(1, 3)) %>%
   select(
     time_point, knowledge_overall, attitude_overall, system_support_score,
-    confidence_score, empathy_score, practice_score
+    confidence_score, empathy_score
   ) %>%
   tbl_summary(
     by = time_point, type = list(
-      system_support_score ~ "continuous",
-      practice_score ~ "continuous"
+      system_support_score ~ "continuous"
     ),
     label = list(
-      knowledge_overall ~ "Knowledge",
-      attitude_overall ~ "Attitude",
-      system_support_score ~ "System Support",
-      confidence_score ~ "Confidence",
-      empathy_score ~ "Empathy",
-      practice_score ~ "Practice"
+      knowledge_overall ~ "Knowledge (43)",
+      attitude_overall ~ "Attitude (102)",
+      system_support_score ~ "System Support (6)",
+      confidence_score ~ "Confidence (40)",
+      empathy_score ~ "Empathy (64)"
     )
   ) %>%
   modify_header(
-    label = "**Domain**",
-    stat_1 = "**Timepoint 1**",
-    stat_2 = "**Timepoint 3**"
+    label = "**Domain (total points)**",
+    stat_1 = "**Baseline**",
+    stat_2 = "**Endline**"
   ) %>%
-  add_n() %>%
   add_p()
 
 comparison_tbl_2 <-
@@ -61,26 +60,23 @@ comparison_tbl_2 <-
   filter(time_point %in% c(2, 3)) %>%
   select(
     time_point, knowledge_overall, attitude_overall, system_support_score,
-    confidence_score, empathy_score, practice_score
+    confidence_score, empathy_score
   ) %>%
   tbl_summary(
     by = time_point, type = list(
-      system_support_score ~ "continuous",
-      practice_score ~ "continuous"
+      system_support_score ~ "continuous"
     ),
     label = list(
-      knowledge_overall ~ "Knowledge",
-      attitude_overall ~ "Attitude",
-      system_support_score ~ "System Support",
-      confidence_score ~ "Confidence",
-      empathy_score ~ "Empathy",
-      practice_score ~ "Practice"
+      knowledge_overall ~ "Knowledge (43)",
+      attitude_overall ~ "Attitude (102)",
+      system_support_score ~ "System Support (6)",
+      confidence_score ~ "Confidence (40)",
+      empathy_score ~ "Empathy (64)"
     )
   ) %>%
   modify_header(
-    label = "**Domain**",
-    stat_1 = "**Timepoint 2**",
-    stat_2 = "**Timepoint 3**"
+    label = "**Domain (total points)**",
+    stat_1 = "**Post-intensive training**",
+    stat_2 = "**Endline**"
   ) %>%
-  add_n() %>%
   add_p()
